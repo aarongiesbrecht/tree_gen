39878aeeeb030162a3decb758a5bf34afa26aecc
aaron <aaronr.giesbrecht@gmail.com>
2019-07-08 22:32:58 -0700

    added working evoOne, changed tile data structure

--- cOne.py
+++ cOne.py
@@ -1,10 +1,29 @@
 #
-# helper functions for evolving a tiles state
+# commits evolution data for tiles that
+# meet requirements based on neighboring
+# tiles data
 #
 
 def evoOne(map, tile):
-    tile.getData(5,5)
-    return 0
+    tile.getData()
+    x=tile.getX()
+    y=tile.getY()
+    evolveCounter=0
     
+    #cycle through the neighbors of the evolving tile
+    for row in range(x-1, x+2):
+        for col in range(y-1, y+2):
+            #if a tile is on the edge of the map it will have a proportionally
+            #lower evolution chance
+            if (0 < row <= 29 and 0 < col <= 29 and
+                (row != x or col != y)): #do not count the tile being evaluated
+                    if (map.getData(row, col) == 1):
+                        evolveCounter += 1
+            
     
+    #if a tile is neighbored by 50% water it floods and becomes water         
+    if (evolveCounter > 5):
+        tile.dataCommit(1)   
+
+#imported after to avoid import loop that took me 10 long minutes to realize    
 from tree_gen import Tile, Map
--- tree_gen.py
+++ tree_gen.py
@@ -25,14 +25,28 @@ class Tile():
     def __init__(self, row, col, data):
         self.row=row
         self.col=col
+        self.strData = ''
         self.data=data
+        self.newData=0
+    #return tile coords
+    def getX(self):
+        return self.row
+    def getY(self):
+        return self.col
     #used by map to manage per tile data
+    def getStr(self):  
+        if (self.data == 0):
+            self.strData = '#'
+        elif (self.data == 1):
+            self.strData = '.'
+        return Label(text=self.strData)
     def getData(self):
-        return Label(text=self.data)
+        return self.data
     #partner to above method
-    def update(self, data):
-        self.data=data
-            
+    def dataCommit(self, data):
+        self.newData=data
+    def dataPush(self):
+        self.data=self.newData            
 
 #a tile managing class
 class Map:
@@ -48,16 +62,20 @@ class Map:
                 t=Tile(row, col, '')
                 self.map[row].append(t)
       
-    #uses Tiles getData/update to manage individual tile data          
+    #uses Tiles getStr/update to manage individual tile data          
+    def getStr(self, row, col):
+        return self.map[row][col].getStr()
     def getData(self, row, col):
         return self.map[row][col].getData()
     def update(self, row, col, data):
-        self.map[row][col].update(data)
+        self.map[row][col].dataCommit(data)
+        self.map[row][col].dataPush()
     def getTile(self, row, col):
         return self.map[row][col]
             
 
-map=Map(15,15)
+#for now map will have a static size definition
+map=Map(30, 30)
 
 #main window is generated from tkinters default frame data
 class Window(Frame):
@@ -92,22 +110,29 @@ class Window(Frame):
      
     #class one evolution, can be run multiple times
     def evolveOne(self):
-        evoOne(map, map.getTile(5,5))
-        map.update(5,5,'X')
-        map.getData(5,5).grid(row=5, column=5, ipadx=5)      
-        
+        #run an evolve check on all tiles
+        for row in range(30):
+            for col in range(30):
+                evoOne(map, map.getTile(row, col))
+        #update all tiles and reprint
+        for row in range(30):
+            for col in range(30):
+                map.getTile(row, col).dataPush()
+                map.getStr(row, col).grid(row=row, column=col, ipadx=5)
+         
         
     #generates a new map based on given paramaters    (paramaters not yet implimented)   
     def createNewGeneration(self):
-        for row in range(0,14):
-            for col in range(0,14):
+        for row in range(30):
+            for col in range(30):
                 r=randint(0,1)
                 if(r == 0):
-                    map.update(row, col, '_')
-                    map.getData(row, col).grid(row=row, column=col, ipadx=5)
+                    map.update(row, col, 0)
+                    map.getStr(row, col).grid(row=row, column=col, ipadx=5)
                 else:
-                    map.update(row, col, '|')
-                    map.getData(row, col).grid(row=row, column=col, ipadx=5)
+                    map.update(row, col, 1)
+                    map.getStr(row, col).grid(row=row, column=col, ipadx=5)
+ 
      
     #displays an img 'i' at x/y    
     def displayImg(self, i, x, y):
@@ -127,7 +152,7 @@ class Window(Frame):
         exit()
 
 root=Tk()
-root.geometry("450x450")
+root.geometry("690x630")
 app=Window(root)
 root.mainloop()
 
